3e39bbdce04e1ee7731223ee58f97cd9b63e99b0
Kim seunggi <94459503+seunggi99@users.noreply.github.com>
2023-04-03 16:59:11 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 436 ms, Memory: 40468 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -0,0 +1,13 @@
+a = int(input())
+
+c = []
+
+for i in range(a):
+    b = input().split()
+    c.append(b)
+
+for i in range(a):
+    for n in range(len(c[i])):
+        print(c[i][n][::-1], end=" ")
+    print()
+           
