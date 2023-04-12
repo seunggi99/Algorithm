c1ccdd6ef3972b09daa95b5c8fd31a8f9bda7dc5
Kim seunggi <94459503+seunggi99@users.noreply.github.com>
2023-04-12 11:39:15 +0900

    [Bronze III] Title: ACM 호텔, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for i in range(t):
+    h, w, n = map(int, input().split())
+    num = n//h + 1
+    f = n % h
+    if n % h == 0:  
+        num = n//h
+        f = h
+    print(f*100+num)
